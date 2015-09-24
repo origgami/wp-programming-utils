bca2be8babb3abceddea1a677b84a4fbe553761b
pablo-pacheco <pablo.pacheco@origgami.com.br>
2015-09-24 16:28:16 -0300

    load this plugin first

--- OriggamiWpProgrammingUtils.php
+++ OriggamiWpProgrammingUtils.php
@@ -17,8 +17,9 @@
 
 namespace OriggamiWpProgrammingUtils;
 
-if ( !class_exists('\OriggamiWpProgrammingUtils\OriggamiWpProgrammingUtils') ) {
-	require dirname(__FILE__) . '/DesignPatterns/Singleton.php';
+if (!class_exists('\OriggamiWpProgrammingUtils\OriggamiWpProgrammingUtils')) {
+	require_once dirname(__FILE__) . '/DesignPatterns/Singleton.php';
+
 //require './oop_functions/OOPFunctions.php';
 
 	/**
@@ -31,11 +32,25 @@ if ( !class_exists('\OriggamiWpProgrammingUtils\OriggamiWpProgrammingUtils') ) {
 		private $autoload;
 		private $oopFunctions;
 		private $utils;
-		
+
 		protected function __construct() {
 			parent::__construct();
+			add_action('activated_plugin', array($this, 'my_plugin_load_first'));
 			$this->loadUsefulFiles();
 			$this->handleAutoload();
+		}
+
+		function my_plugin_load_first() {
+			//$path	 = str_replace(WP_PLUGIN_DIR . '/', '', __FILE__);
+			$path	 = plugin_basename(dirname(__FILE__)) . '/' . basename(__FILE__);
+			$plugins = get_option('active_plugins');
+			if ($plugins) {
+				if ($key = array_search($path, $plugins)) {
+					array_splice($plugins, $key, 1);
+					array_unshift($plugins, $path);
+					update_option('active_plugins', $plugins);
+				}
+			}
 		}
 
 		private function loadUsefulFiles() {
@@ -44,9 +59,9 @@ if ( !class_exists('\OriggamiWpProgrammingUtils\OriggamiWpProgrammingUtils') ) {
 		}
 
 		private function handleAutoload() {
-			if(!$this->getAutoload()){
-				$autoload = new Autoload\WpAutoload(array(get_stylesheet_directory(), WP_PLUGIN_DIR));
-				$dir = basename(dirname(__FILE__));
+			if (!$this->getAutoload()) {
+				$autoload	 = new Autoload\WpAutoload(array(get_stylesheet_directory(), WP_PLUGIN_DIR));
+				$dir		 = basename(dirname(__FILE__));
 				$autoload->addNamespaceReplace(array('OriggamiWpProgrammingUtils' => $dir));
 				$autoload->handleAutoload();
 				$this->setAutoload($autoload);
@@ -61,7 +76,7 @@ if ( !class_exists('\OriggamiWpProgrammingUtils\OriggamiWpProgrammingUtils') ) {
 			return $this->autoload;
 		}
 
-		function setAutoload( Autoload\WpAutoload $autoload ) {
+		function setAutoload(Autoload\WpAutoload $autoload) {
 			$this->autoload = $autoload;
 		}
 
@@ -70,13 +85,13 @@ if ( !class_exists('\OriggamiWpProgrammingUtils\OriggamiWpProgrammingUtils') ) {
 		 * @return Utils\Utils
 		 */
 		function getUtils() {
-			if ( !$this->utils ) {
+			if (!$this->utils) {
 				$this->setUtils(new Utils\Utils());
 			}
 			return $this->utils;
 		}
 
-		function setUtils( Utils\Utils $utils ) {
+		function setUtils(Utils\Utils $utils) {
 			$this->utils = $utils;
 		}
 
